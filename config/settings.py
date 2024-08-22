313d2313d54c996bc172c92f961e6dd02e32ed5c
pemidi <rashidi.peyman@gmail.com>
2024-08-23 01:52:58 +0330

    Override default user model in settings.py

--- config/settings.py
+++ config/settings.py
@@ -126,3 +126,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# The django default user overrode and set to settings 👥 ⚙️
+AUTH_USER_MODEL = 'accounts.CustomUser'
