8630de830172fb669f7c7713e3a2ee5c27620993
pemidi <rashidi.peyman@gmail.com>
2024-08-27 02:11:08 +0330

    fix: Include `access_token` in the response payload

--- accounts/views.py
+++ accounts/views.py
@@ -23,7 +23,11 @@ class AuthenticateAPIView(APIView):
         if serializer.is_valid():
             user = serializer.validate(request.data)
             refresh = RefreshToken.for_user(user)
-            return Response(data={'refresh': str(refresh)}, status=status.HTTP_200_OK)
+            return Response(data={
+                'refresh': str(refresh),
+                'access': str(refresh.access_token)
+            },
+                status=status.HTTP_200_OK)
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def _handle_register(self, request):
@@ -31,5 +35,8 @@ class AuthenticateAPIView(APIView):
         if serializer.is_valid():
             user = serializer.save()
             refresh = RefreshToken.for_user(user)
-            return Response(data={'refresh': str(refresh)}, status=status.HTTP_201_CREATED)
+            return Response(data={
+                'refresh': str(refresh),
+                'access': str(refresh.access_token)
+            }, status=status.HTTP_201_CREATED)
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
