e38ac2feccf4a2f096d6c13b175b2a658c37d633
pemidi <rashidi.peyman@gmail.com>
2024-08-29 11:28:07 +0330

    Feat: register `CustomUser` with fully features

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from accounts.models import CustomUser
 
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name')
+    list_display = ('username', 'email', 'first_name', 'last_name', 'role')
     search_fields = ('username', 'email', 'first_name', 'last_name')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'role')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'role'),
+        }),
+    )
+    filter_horizontal = ('user_permissions',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
