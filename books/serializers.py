dc842ccc15e50549d94bbdd6d71c805d60d8dcb4
pemidi <rashidi.peyman@gmail.com>
2024-08-29 04:22:15 +0330

    Feat: add `RetrieveBookAPIView`, added an API to represent book detail with annotate data

--- books/serializers.py
+++ books/serializers.py
@@ -1,6 +1,51 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
-from books.models import Book, Author, Bookmark
+from books.models import Book, Author, Bookmark, Review
+
+
+class BookReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['comment', 'score']
+
+
+class UserReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['user', 'comment', 'score']
+
+    def get_username(self, obj):
+        return obj.user.username
+
+
+class BookDetailSerializer(serializers.ModelSerializer):
+    reviews = BookReviewSerializer(many=True)
+    reviews_count = serializers.IntegerField(read_only=True)
+    average_score = serializers.IntegerField(read_only=True)
+    user_reviews = serializers.SerializerMethodField()
+    score_1_count = serializers.IntegerField(read_only=True)
+    score_2_count = serializers.IntegerField(read_only=True)
+    score_3_count = serializers.IntegerField(read_only=True)
+    score_4_count = serializers.IntegerField(read_only=True)
+    score_5_count = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Book
+        fields = ['title',
+                  'reviews',
+                  'user_reviews',
+                  'reviews_count',
+                  'average_score',
+                  'score_1_count',
+                  'score_2_count',
+                  'score_3_count',
+                  'score_4_count',
+                  'score_5_count']
+
+    def get_user_reviews(self, obj):
+        reviews = obj.reviews.select_related('user').all()
+        return UserReviewSerializer(reviews, many=True).data
 
 
 class BookSerializer(serializers.ModelSerializer):
--- books/views.py
+++ books/views.py
@@ -1,8 +1,9 @@
-from rest_framework.generics import ListAPIView
-from django.db.models import Count, Exists, OuterRef
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from django.db.models import Count, Exists, OuterRef, Avg, Case, When, IntegerField
+
 from books.models import Book, Bookmark
 from books.paginations import BookStandardPagination
-from books.serializers import BookSerializer, AuthenticatedUserBookSerializer
+from books.serializers import BookSerializer, AuthenticatedUserBookSerializer, BookDetailSerializer
 
 
 class BookListAPIView(ListAPIView):
@@ -26,3 +27,18 @@ class BookListAPIView(ListAPIView):
             return AuthenticatedUserBookSerializer
         else:
             return BookSerializer
+
+
+class RetrieveBookAPIView(RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookDetailSerializer
+
+    def get_queryset(self):
+        return super().get_queryset().prefetch_related('reviews').annotate(
+            score_1_count=Count(Case(When(reviews__score=1, then=1), output_field=IntegerField())),
+            score_2_count=Count(Case(When(reviews__score=2, then=1), output_field=IntegerField())),
+            score_3_count=Count(Case(When(reviews__score=3, then=1), output_field=IntegerField())),
+            score_4_count=Count(Case(When(reviews__score=4, then=1), output_field=IntegerField())),
+            score_5_count=Count(Case(When(reviews__score=5, then=1), output_field=IntegerField())),
+            reviews_count=Count('reviews'), average_score=Avg('reviews__score')
+        )
